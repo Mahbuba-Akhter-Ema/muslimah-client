fce6fcf4017aa54d363bb375f761617d119f8903
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-08 04:47:20 +0600

    added services section

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch('services.json')
+    .then(res => res.json)
+    .then(data => setServices(data));
+  },[])
+    return (
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 bg-zinc-900 mt-10 rounded">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <div>
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
+            Connect With Us
+          </p>
+        </div>
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-white lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="bc9273ce-29bb-4565-959b-714607d4d027"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#bc9273ce-29bb-4565-959b-714607d4d027)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative">Connect</span>
+          </span>{' '}
+          with Muslim Service Provider.
+        </h2>
+        <p className="text-base text-white md:text-lg">
+        Book video consultations with scholars, practitioners, financial advisors, and educators worldwide.
+        </p>
+      </div>
+      <div className="grid max-w-md gap-10 row-gap-8 lg:max-w-screen-lg sm:row-gap-10 lg:grid-cols-3 xl:max-w-screen-lg sm:mx-auto">
+        <h2 className='text-white'>{services.length}</h2>
+      </div>
+    </div>
+    );
+};
+
+export default Services;
