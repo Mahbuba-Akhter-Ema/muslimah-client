3e3d90f9b972dd5b022056bdad6eaddc07c90893
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-08 11:37:16 +0600

    servicecard section completed

--- src/Pages/Home/ServicesCard/ServicesCard.js
+++ src/Pages/Home/ServicesCard/ServicesCard.js
@@ -1,35 +1,43 @@
 import React from 'react';
+import { FaDollarSign, FaGrinStars} from 'react-icons/fa';
 
-const ServicesCard = ({service}) => {
-  const {img, title, price, rating, description} = service;
-    return (
-        <div className="flex flex-col transition duration-300 bg-white rounded shadow-sm hover:shadow">
-          <div className="relative w-full h-48">
-            <img
-              src={img}
-              className="object-cover w-full h-full rounded-t"
-              alt="Plan"
-            />
+const ServicesCard = ({ service }) => {
+  const { img, title, price, rating, description } = service;
+  return (
+    <div className="flex flex-col transition duration-300 bg-white rounded shadow-sm hover:shadow">
+      <div className="relative w-full h-48">
+        <img
+          src={img}
+          className="object-cover w-full h-full rounded-t"
+          alt="Plan"
+        />
+      </div>
+      <div className="flex flex-col justify-between flex-grow p-4 border border-t-0 rounded-b">
+        <div>
+          <div className="text-lg font-bold">{title}</div>
+          {
+            <p className="text-sm text-gray-900">
+              {description.slice(0, 100)+ '...' }
+            </p>
+          }
+          <div className="flex justify-items-center justify-around p-2 mt-5 mb-4 font-bold sm:text-3xl">
+          <div className='flex items-center'>
+          <span className='text-2xl'><FaDollarSign /></span> {price}
           </div>
-          <div className="flex flex-col justify-between flex-grow p-8 border border-t-0 rounded-b">
-            <div>
-              <div className="text-lg font-semibold">{title}</div>
-              <p className="text-sm text-gray-900">
-                {description}
-              </p>
-              <div className="mt-1 mb-4 mr-1 text-4xl font-bold sm:text-5xl">
-                $12
-              </div>
-            </div>
-            <a
-              href="/"
-              className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none"
-            >
-              Buy Basic
-            </a>
+          <div className='flex items-center'>
+          <span className='text-2xl mr-2'><FaGrinStars /></span> {rating}
           </div>
+          </div>       
         </div>
-    );
+        <a
+          href="/"
+          className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none"
+        >
+          Read More
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ServicesCard;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,7 +4,7 @@ import logo from './logo.png';
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <div class="bg-black rounded">
+        <div class="bg-black rounded relative z-40">
             <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
                 <div class="relative flex items-center justify-between">
                     <a
@@ -168,7 +168,7 @@ const Header = () => {
                                             <li>
                                                 <a
                                                     href="/"
-                                                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                                                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-400 hover:bg-purple-700 focus:shadow-outline focus:outline-none"
                                                     aria-label="Sign up"
                                                     title="Sign up"
                                                 >
