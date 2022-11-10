a0c22d15928e8d55b850c5dd5a3a7323a76fdf71
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-10 21:28:42 +0600

    changes on use title file

--- src/Hook/UseTitle.js
+++ src/Hook/UseTitle.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 const useTitle = title =>{
     useEffect(()=>{
-        document.title = `${title} -titlechanges`;
+        document.title = `${title} -Muslimah`;
     },[title])
 }
 
